50d96d73a89efcb0cf550d017636860e0268e898
Haseeb Ahmad <42300494+hahmad748@users.noreply.github.com>
2022-12-03 19:54:34 +0500

    Laravel 9 support with vite

--- src/views/layouts/headLinks.blade.php
+++ src/views/layouts/headLinks.blade.php
@@ -9,7 +9,8 @@
 {{-- scripts --}}
 <script src="{{ asset('js/devschat/font.awesome.min.js') }}"></script>
 <script src="{{ asset('js/devschat/autosize.js') }}"></script>
-<script src="{{ asset('js/app.js') }}"></script>
+@vite(['resources/sass/app.scss', 'resources/js/app.js'])
+<script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
 <script src='https://unpkg.com/nprogress@0.2.0/nprogress.js'></script>
 <script src="https://cdn.socket.io/socket.io-3.0.1.min.js"></script>
 
@@ -18,7 +19,6 @@
 <link rel='stylesheet' href='https://unpkg.com/nprogress@0.2.0/nprogress.css'/>
 <link href="{{ asset('css/devschat/style.css') }}" rel="stylesheet" />
 <link href="{{ asset('css/devschat/'.$dark_mode.'.mode.css') }}" rel="stylesheet" />
-<link href="{{ asset('css/app.css') }}" rel="stylesheet" />
 
 {{-- Messenger Color Style--}}
 @include('DevsFort::layouts.messengerColor')
